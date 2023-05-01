665b8222b45389c2b7b46644c7a11425984e7696
Cyril Pulver <cyril.pulver@epfl.ch>
2023-05-01 12:57:15 +0200

    N_from_tsv.R

---
+++ R/N_from_tsv.R
@@ -0,0 +1,14 @@
+#' fast loading of N from a .tsv, using data.table
+#' 
+#' loads N specified in a .tsv file, and returns it as a matrix ready for use in preprocess_E_N_for_activities()
+#'
+#' @param filename path to tsv file specifying N, with genes as rows and TE subfamilies as columns
+#' @return matrix N
+#' @export
+N_from_tsv <- function(filename) {
+	N = data.table:fread(filename, header=T, sep='\t', stringsAsFactors = F, data.table=FALSE):
+	rownames(N) = N$V1
+	N$V1 = NULL
+	colnames(N) = gsub(pattern = '-', replacement = '.', x = colnames(N))
+	return(as.matrix(N))
+}
