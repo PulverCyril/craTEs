eea7cdff7604df3ffb5f31dae012f14c65b55cd8
Cyril Pulver <44056089+PulverCyril@users.noreply.github.com>
2023-12-13 10:58:28 +0100

    Update preprocess_E_N_for_activities.R

--- R/preprocess_E_N_for_activities.R
+++ R/preprocess_E_N_for_activities.R
@@ -13,16 +13,19 @@
 #' @param preds_protected vector containing the names of predictors that cannot be filtered out based on N_threshold
 #' @param redownload_exons TRUE: exons are redownloaded to compute TPMs. FALSE: exons are not redownloaded to compute TPMs.
 #' @param exons_dir directory where gene models are saved to avoid a redownload
+#' @param org reference assembly, one of hg19 or hg38
 #' @return a list object with a centered expression matrix and a filtered N matrix
 #' @export
-preprocess_E_N_for_activities <- function(count_table, N, 
+preprocess_E_N_for_activities <- function(count_table, 
+					  N, 
                                           count_threshold=c(10, 1), 
                                           N_threshold = 150,
                                           pseudocount_quantile=0.05,
                                           log_tpm_plot_path,
                                           preds_protected=NULL,
                                           redownload_exons=FALSE,
-					  exons_dir = "../data/temp/"
+					  exons_dir = "../data/temp/",
+					  org = 'hg19'
                                           ) {
     stopifnot(is.matrix(count_table) & all(count_table>=0))
     stopifnot(is.matrix(N) & all(N>=0))
@@ -62,7 +65,7 @@ preprocess_E_N_for_activities <- function(count_table, N,
     count_table = get_pseudocounts(count_table, pseudocount_quantile)
 
     # computing log2(tpm), keeping them for the graph
-    log_tpm = log2(get_tpm_local(count_table, redownload_exons=redownload_exons, exons_dir = exons_dir))
+    log_tpm = log2(get_tpm_local(count_table, org=org, redownload_exons=redownload_exons, exons_dir = exons_dir))
     gc()
 
     # plotting log tpm for quality control
